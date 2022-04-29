0e0f45431460275ab46f29ec3683b41bbf7269b0
theoriginalvisagie <67701001+theoriginalvisagie@users.noreply.github.com>
2022-04-29 11:01:03 +0200

    Added click listener

--- Section 1/first-app/src/app/home/home.page.ts
+++ Section 1/first-app/src/app/home/home.page.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  text = "Default starting text";
+  onChangeText(){
+    this.text = "Text changed";
+  }
 
 }
